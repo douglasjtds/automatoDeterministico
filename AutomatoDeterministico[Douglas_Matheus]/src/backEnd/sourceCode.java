0dd1861619b8720b0e42d1947ba45cec1b4b6b90
douglasjtds <douglasjtds@gmail.com>
2017-06-07 02:00:11 -0300

    752727425oi

--- AutomatoDeterministico[Douglas_Matheus]/src/backEnd/sourceCode.java
+++ AutomatoDeterministico[Douglas_Matheus]/src/backEnd/sourceCode.java
@@ -11,7 +11,7 @@ public class sourceCode {
     public void code(String aut){
         //TODO COLOCAR O BACKEND AQUI:
         //estado inicial + - o número
-        int state[] = {0,1,2,3,4,5,6,8,9}; //será que é isso?
+        int allowedState[] = {0,1,2,3,4,5,6,8,9}; //será que é isso?
         if(true != aut.isEmpty()){
             JOptionPane.showMessageDialog(null, "Sim! ");
         } else{
